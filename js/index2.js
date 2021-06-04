1d5d6fdbd9ee9b9ff92a1a9430d6a4735f58d7c8
wellchis <chisu1995@naver.com>
2021-06-04 15:18:25 +0900

    index2

---
+++ js/index2.js
@@ -0,0 +1,22 @@
+// var obj = $("#view");
+// 숨기기
+function display(){
+    var a = document.getElementByld("view");
+    if(a.style.display=='none'){
+      a.style.display='block';
+    } else {
+      a.style.display='none';
+    }
+
+}
+
+// 보이기
+// function Ondisplay(){
+// document.getElementByld("view").style.display="block";
+// }
+// function view(){
+//     $("#view");.hide();
+//   }
+// function view2(){
+//     $("#view");.show();
+//   }
