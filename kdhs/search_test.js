f97df8bb7869cd128edfb0bcb4365d9f124a2fc1
Donghop <dh1996m@naver.com>
2021-06-28 00:53:11 +0900

    동호꺼

---
+++ kdhs/search_test.js
@@ -0,0 +1,16 @@
+
+
+function OpenPop() {
+	document.getElementById("popup_layer").style.display = "";
+}
+function OpenPopimg() {
+	document.getElementById("img_popup_layer").style.display = "";
+}
+function closePop() {
+	document.getElementById("popup_layer").style.display = "none";
+
+}
+function closePopimg() {
+	document.getElementById("img_popup_layer").style.display = "none";
+
+}
