cf8c8ba878d9ed6086eb3bb85ee3c79944ed7770
kjmyeong <cptm2399@gmail.com>
2021-06-29 17:19:35 +0900

    search

--- js/search_IN.js
+++ js/search_IN.js
@@ -1,15 +1,44 @@
-function test(){
-  document.getElementById("demo").style.color = "red";
+function doDisplay1(){
+
+  var text = document.getElementById("horror");
+  var text2 = document.getElementById("comedy");
+  var text3 = document.getElementById("mello");
+
+  if(text.style.display == 'none'){
+      text.style.display = 'block';
+      text2.style.display = 'none';
+      text3.style.display = 'none';
+  } else {
+      text.style.display = 'none';
+  }
 }
 
-function doDisplay() {
-  var horror = document.getElementById("horror");
-  var comedy = document.getElementById("comedy");
+function doDisplay2(){
 
-  if(horror.style.display == 'none'){
-    horror.style.display = 'block';
+  var text = document.getElementById("horror");
+  var text2 = document.getElementById("comedy");
+  var text3 = document.getElementById("mello");
 
-  }else{
-    horror.style.display = 'none';
+  if(text2.style.display == 'none'){
+    text.style.display = 'none';
+    text2.style.display = 'block';
+    text3.style.display = 'none';
+  } else {
+    text2.style.display = 'none';
+  }
+}
+
+function doDisplay3(){
+
+  var text = document.getElementById("horror");
+  var text2 = document.getElementById("comedy");
+  var text3 = document.getElementById("mello");
+
+  if(text3.style.display == 'none'){
+    text.style.display = 'none';
+    text2.style.display = 'none';
+    text3.style.display = 'block';
+  } else {
+    text3.style.display = 'none';
   }
 }
