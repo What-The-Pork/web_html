12cc0325ab24e4519837734893f84f7426bda8c7
wellchis <chisu1995@naver.com>
2021-07-06 01:21:53 +0900

    html

--- js/login_btn.js
+++ js/login_btn.js
@@ -1,12 +1,39 @@
-window.onload = function(){
+// window.onload = function(){
+//
+//   var display = document.getElementById("profile_wrap");
+//   var btn = document.getElementById("login_btn");
+//
+//   btn.onclick = function(){
+//     if(display.style.display=="none"){
+//       display.style.display = "block";
+//     } else {
+//       display.style.display = "none";
+//     }
+//   }
+// }
+
+var a;
+function login_btn(){
   var display = document.getElementById("profile_wrap");
   var btn = document.getElementById("login_btn");
 
-  btn.onclick = function(){
-    if(display.style.display=="none"){
-      display.style.display = "block";
-    } else {
-      display.style.display = "none";
-    }
+  if(a==1){
+    display.style.display="block";
+    return a=0;
+  } else {
+    display.style.display="none";
+    return a=1;
   }
 }
+
+// let btn = document.getElementById("login_btn");
+//
+// btn.onclick = display;
+//
+// function display(){
+//     if(document.getElementById("profile_wrap").style.display=="none"){
+//       document.getElementById("profile_wrap").style.display = "block";
+//     } else {
+//       document.getElementById("profile_wrap").style.display = "none";
+//     }
+// }
